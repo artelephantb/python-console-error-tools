bff9fbe0457ed85055a8b604a669796bc505df1d
Artitapheiont <87944666+artelephantb@users.noreply.github.com>
2025-08-27 21:34:24 -0400

    Added note

--- console_error_tools.py
+++ console_error_tools.py
@@ -5,6 +5,12 @@ def finish(catagory, *message, hint=''):
 	print('\033[96m*', '~' * 64, '*\033[0m')
 	exit(0)
 
+def note(catagory, *message, hint=''):
+	print('\033[96m*', '~' * 64, '*\033[0m')
+	print('\033[1m\033[95m' + catagory + '\033[0m: \033[95m' + ' '.join(message) + '\033[0m')
+	if hint: print('-->', hint, '<--')
+	print('\033[96m*', '~' * 64, '*\033[0m')
+
 def warning(catagory, *message, hint=''):
 	print('\033[93m*', '~' * 64, '*\033[0m')
 	print('\033[1m\033[95m' + catagory + '\033[0m: \033[95m' + ' '.join(message) + '\033[0m')
