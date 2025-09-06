b4682c9b448fd1275fd81414d9c5bd6aa0d8ab06
Artitapheiont <87944666+artelephantb@users.noreply.github.com>
2025-09-06 15:59:25 -0400

    Made format more readable

--- console_error_tools.py
+++ console_error_tools.py
@@ -1,36 +1,45 @@
-def finish(catagory, *message, hint=''):
+TEXT_RED = '\033[91m'
+TEXT_YELLOW = '\033[93m'
+TEXT_PINK = '\033[95m'
+TEXT_CYAN = '\033[96m'
+TEXT_GREEN = '\033[92m'
+
+FORMAT_BOLD = '\033[1m'
+FORMAT_END = '\033[0m'
+
+def finish(catagory: str, *message: str, hint: str = ''):
 	'''Shows a final message and exits the program'''
-	print('\033[96m*', '~' * 64, '*\033[0m')
-	print('\033[1m\033[92m' + catagory + '\033[0m: \033[92m' + ' '.join(message) + '\033[0m')
+	print(TEXT_CYAN + '*', '~' * 64, '*' + FORMAT_END)
+	print(FORMAT_BOLD + TEXT_GREEN + catagory + FORMAT_END + ': ' + TEXT_GREEN + ' '.join(message) + FORMAT_END)
 	if hint: print('-->', hint, '<--')
-	print('\033[96m*', '~' * 64, '*\033[0m')
+	print(TEXT_CYAN + '*', '~' * 64, '*' + FORMAT_END)
 	exit(0)
 
-def note(catagory, *message, hint=''):
+def note(catagory: str, *message: str, hint: str = ''):
 	'''Shows a general message'''
-	print('\033[96m*', '~' * 64, '*\033[0m')
-	print('\033[1m\033[95m' + catagory + '\033[0m: \033[95m' + ' '.join(message) + '\033[0m')
+	print(TEXT_CYAN + '*', '~' * 64, '*' + FORMAT_END)
+	print(FORMAT_BOLD + TEXT_PINK + catagory + FORMAT_END + ': ' + TEXT_PINK + ' '.join(message) + FORMAT_END)
 	if hint: print('-->', hint, '<--')
-	print('\033[96m*', '~' * 64, '*\033[0m')
+	print(TEXT_CYAN + '*', '~' * 64, '*' + FORMAT_END)
 
-def warning(catagory, *message, hint=''):
+def warning(catagory: str, *message: str, hint: str = ''):
 	'''Shows a warning message'''
-	print('\033[93m*', '~' * 64, '*\033[0m')
-	print('\033[1m\033[95m' + catagory + '\033[0m: \033[95m' + ' '.join(message) + '\033[0m')
+	print(TEXT_YELLOW + '*', '~' * 64, '*' + FORMAT_END)
+	print(FORMAT_BOLD + TEXT_PINK + catagory + FORMAT_END + ': ' + TEXT_PINK + ' '.join(message) + FORMAT_END)
 	if hint: print('-->', hint, '<--')
-	print('\033[93m*', '~' * 64, '*\033[0m')
+	print(TEXT_YELLOW + '*', '~' * 64, '*' + FORMAT_END)
 
-def fumble(catagory, *message, hint=''):
+def fumble(catagory: str, *message: str, hint: str = ''):
 	'''Shows a error message without exiting the program'''
-	print('\033[93m*', '~' * 64, '*\033[0m')
-	print('\033[1m\033[91m' + catagory + '\033[0m: \033[91m' + ' '.join(message) + '\033[0m')
+	print(TEXT_YELLOW + '*', '~' * 64, '*' + FORMAT_END)
+	print(FORMAT_BOLD + TEXT_RED + catagory + FORMAT_END + ': ' + TEXT_RED + ' '.join(message) + FORMAT_END)
 	if hint: print('-->', hint, '<--')
-	print('\033[93m*', '~' * 64, '*\033[0m')
+	print(TEXT_YELLOW + '*', '~' * 64, '*' + FORMAT_END)
 
-def error(catagory, *message, hint=''):
+def error(catagory: str, *message: str, hint: str = ''):
 	'''Shows a error message and exits the program'''
-	print('\033[95m*', '~' * 64, '*\033[0m')
-	print('\033[1m\033[91m' + catagory + '\033[0m: \033[91m' + ' '.join(message) + '\033[0m')
+	print(TEXT_PINK + '*', '~' * 64, '*' + FORMAT_END)
+	print(FORMAT_BOLD + TEXT_RED + catagory + FORMAT_END + ': ' + TEXT_RED + ' '.join(message) + FORMAT_END)
 	if hint: print('-->', hint, '<--')
-	print('\033[95m*', '~' * 64, '*\033[0m')
+	print(TEXT_PINK + '*', '~' * 64, '*' + FORMAT_END)
 	exit(1)
